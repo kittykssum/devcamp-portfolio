8f785c61f4cd90d7d08ae8828935068d067cdd26
kittykssum <kittykssum@gmail.com>
2020-02-21 16:23:08 +0800

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
   #get 'pages/home'
   #get 'pages/about'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   #get 'pages/contact'
   get 'contact', to: 'pages#contact'
