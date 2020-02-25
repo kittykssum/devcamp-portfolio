d3a050c8c237b304497dec485d442a8e697e873d
kittykssum <kittykssum@gmail.com>
2020-02-25 11:26:38 +0800

    implemented custom scope for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
   #get 'pages/home'
   #get 'pages/about'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
+
+puts "3 Topics created"
+
+
 10.times do |blog|
 	Blog.create!(
 		title: "My Blog Post #{blog}",
-		body: "今次的抵食優惠"
+		body: "今次的抵食優惠",
+		topic_id: Topic.last.id
 	)
+	
 end
 
 puts "10 blog posts created"
@@ -16,14 +27,24 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "My great service",
+		subtitle: "Ruby on Rails",
 		body: "札幌雪まつりは",
 		main_image: "https://via.placeholder.com/600x400",
 		thumb_image: "https://via.placeholder.com/350x200"
 	)
 end
 
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Angular",
+		body: "札幌雪まつりは",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/350x200"
+	)
+end
 puts "9 portfolio items created"
