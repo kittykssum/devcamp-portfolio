73188c067a86ade3c525c6ffc09d0f2741e80131
kittykssum <kittykssum@gmail.com>
2020-02-28 14:02:14 +0800

    helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def sample_helper
-		"<p>i am a sample helper, shared for views, run ruby to dym create html content</p>".html_safe
+		content_tag(:div, "sample helper shared for views, ruby dym content_tag", class: "my-class")
 	end
 	def login_helper
 		if current_user.is_a?(User)
@@ -11,4 +11,10 @@ module ApplicationHelper
 	      (link_to "Login", new_user_session_path)
 	    end
 	end
+	def source_helper(layout_name)
+		if session[:source]
+			greeting = "Thanks for ving me from #{session[:source]} on #{layout_name}"
+      		content_tag(:p, greeting, class: "source-greeting")
+      	end
+    end
 end
